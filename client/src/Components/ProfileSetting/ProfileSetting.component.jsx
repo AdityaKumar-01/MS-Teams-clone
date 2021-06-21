85907f4b6a16bc7af14bab58ebca608e2e3e6c26
aditya <aditya7238195@gmail.com>
2021-06-21 15:37:07 +0530

    user can now update username"

--- client/src/Components/ProfileSetting/ProfileSetting.component.jsx
+++ client/src/Components/ProfileSetting/ProfileSetting.component.jsx
@@ -1,11 +1,98 @@
-import React from 'react';
+import React, { useState, useRef } from "react";
+import Avatar from "react-avatar";
+import axios from "axios";
+import "./ProfileSetting.styles.css";
+const ProfileSetting = () => {
+  const [userNameStatus, setUserNameStatus] = useState("");
+  const [pwdStatus, setPwdStaus] = useState("");
 
-const ProfileSetting= () => {
-    return (
-        <div>
-            <h1>Settings</h1>
+  const userName = useRef();
+  const pwd = useRef();
+  const conPwd = useRef();
+
+  const updateUser = () => {
+    const user = {
+      newName: userName.current.value,
+      name:localStorage.getItem("userName"),
+      password: localStorage.getItem("password")
+    };
+    // Sending data to backend to check user exists else enroll the user
+    axios
+      .post("/user/signIn", user)
+      .then((data) => {
+        setUserNameStatus(data.data.msg);
+        if (data.data.status === 201) {
+          // Store the data to local storage to avoid user logging every time
+          localStorage.setItem("userName", userName.current.value);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    e.target.className === "update-user-name" ? updateUser() : console.log("pwd");
+  };
+  return (
+    <div className="profile-settings">
+      <div className="profile-overview">
+        <span>
+          <Avatar
+            name={localStorage.getItem("userName")}
+            size="80"
+            className="card-avatar"
+            round={true}
+          />
+        </span>
+        <span className="profile-name">{localStorage.getItem("userName")}</span>
+      </div>
+
+      <div className="update-form-container">
+        <div className="update-form">
+          <span className="profile-update-title">Update Username</span>
+          <span>{userNameStatus}</span>
+          <form onSubmit={(e) => handleUpdate(e)} className="update-user-name">
+            <input
+              type="text"
+              placeholder="Enter new username"
+              required
+              ref={userName}
+            />
+            <button className="create-chat-btn">Update</button>
+          </form>
         </div>
-    );
-}
+        <div className="create-chat-or-col">
+          <hr width="1" size="100" />
+          OR
+          <hr width="1" size="100" />
+        </div>
+        <div className="update-form">
+          <span className="profile-update-title">Update Password</span>
+          <span>{pwdStatus}</span>
+          <form onSubmit={(e) => handleUpdate(e)} className="update-user-pwd">
+            <input
+              type="password"
+              placeholder="Enter new password"
+              required
+              ref={pwd}
+            />
+            <input
+              type="password"
+              placeholder="Confirm new password"
+              required
+              ref={conPwd}
+            />
+            <button className="create-chat-btn">Update</button>
+          </form>
+        </div>
+      </div>
+      <div>
+        <button className="create-chat-btn">Log Out</button>
+      </div>
+    </div>
+  );
+};
 
 export default ProfileSetting;
--- server/Routes/user.routes.js
+++ server/Routes/user.routes.js
@@ -31,7 +31,7 @@ router.post("/signUp", (req, res) => {
         secret: req.body.password,
       };
 
-      // config object with API call methid and header
+      // config object with API call method and header
       var config = {
         method: "post",
         url: "https://api.chatengine.io/users/",
@@ -49,6 +49,7 @@ router.post("/signUp", (req, res) => {
         })
         .catch(function (error) {
           console.log(error);
+
           // if any error occurred send 400 status and error msg to be displayd
           res.json({ status: 400, msg: "User Already Exists" });
         });
@@ -58,12 +59,49 @@ router.post("/signUp", (req, res) => {
 
 router.post("/signIn", (req, res) => {
   // check user exist with same username or not
+
   user.find({ userName: req.body.name }, (err, data) => {
     // if you get 1 document then there exist a user with this name
     if (data.length >= 1 && sha256(req.body.password) === data[0].password) {
       // Notify the frontend that the user is signedIn
       res.json({ status: 201, msg: "User Found" });
     } else res.json({ status: 404, msg: "Check your credentials" }); // if any error occurred send 404 status and error msg to be displayd
+  });
+});
+
+router.post("/updateUserName", (req, res) => {
+  user.find({ userName: req.body.newName }, (err, data) => {
+    if (data.length >= 1) res.json({ status: 403, msg: "User name taken" });
+    else {
+      var data = {
+        username: req.body.newName,
+      };
+
+      // config object with API call meth0d and header
+      var config = {
+        method: "patch",
+        url: "https://api.chatengine.io/users/me/",
+        headers: {
+          "Project-ID": process.env.CHAT_ENGINE_PROJECT_ID,
+          "User-Name": req.body.name,
+          "User-Secret": req.body.password,
+        },
+        data: data,
+      };
+
+      // Make the call
+      axios(config)
+        .then(function (response) {
+          // Notify the frontend that the user is being created
+          res.json({ status: 200, msg: "User name updated" });
+        })
+        .catch(function (error) {
+          console.log(error);
+          // if any error occurred send 400 status and error msg to be displayd
+          // res.json({ status: 403, msg: "User Already Exists" });
+          console.log(error);
+        });
+    }
   });
 });
 
